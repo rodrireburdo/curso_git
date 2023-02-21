4d7fc1ce7695b7150b4089c1463324676886aab3
Rodrigo Reburdo <rodri.reburdo01@gmail.com>
2023-02-21 11:34:55 -0300

    Introduccion DNI

--- prueba.py
+++ prueba.py
@@ -1,8 +1,10 @@
-
+from time import sleep
 
 nombre = input("Introduce tu nombre: ")
 print("Bienvenido {}".format(nombre))
 edad = int(input("Introduzca su edad: "))
+
+sleep(1)
 
 if edad > 0:
     if edad > 18:
@@ -11,3 +13,9 @@ if edad > 0:
         print("Te faltan {} años para ser mayor de edad...".format(18-edad))
 else:
     print("Edad invalida... ")
+
+documento = int(input("Ingrese su numero de documento: "))
+if len(documento) == 8:
+    print("Su incripcion a terminado")
+else:
+    print("Error en el numero de documeno") 
