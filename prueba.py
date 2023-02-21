cedd9dc94573178565dc8d4cf35230985e03edca
Rodrigo Reburdo <rodri.reburdo01@gmail.com>
2023-02-21 12:14:05 -0300

    añadido funcion main

--- prueba.py
+++ prueba.py
@@ -1,21 +1,25 @@
 from time import sleep
 
-nombre = input("Introduce tu nombre: ")
-print("Bienvenido {}".format(nombre))
-edad = int(input("Introduzca su edad: "))
+def main ():
+    print("Nos alegra que crees una cuenta con nosotros\n")
+    nombre = input("Introduce tu nombre: ")
+    print("Bienvenido {}".format(nombre))
+    edad = int(input("Introduzca su edad: "))
 
-sleep(1)
+    sleep(1)
 
-if edad > 0:
-    if edad > 18:
-        print("Eres mayor de edad")
+    if edad > 0:
+        if edad >= 18:
+            print("Eres mayor de edad")
+        else:
+            print("Te faltan {} años para ser mayor de edad...".format(18-edad))
     else:
-        print("Te faltan {} años para ser mayor de edad...".format(18-edad))
-else:
-    print("Edad invalida... ")
+        print("Edad invalida... ")
 
-documento = int(input("Ingrese su numero de documento: "))
-if len(documento) == 8:
-    print("Su incripcion a terminado")
-else:
-    print("Error en el numero de documeno") 
+    documento = int(input("Ingrese su numero de documento: "))
+
+    print("DNI: {}".format(documento))
+
+__name__ == "__ main __ "
+main () 
+
